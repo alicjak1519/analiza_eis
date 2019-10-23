261e54f81fc3955503add1fbbf3e43398e70b558
Alicja <akrzeminska1519@gmail.com>
2019-10-23 18:34:03 +0200

    Proba ograniczenia czestotliwosci i zapisu do pliku

--- uwzglednij_zakres_czestotliwosci.m
+++ uwzglednij_zakres_czestotliwosci.m
@@ -1,17 +1,27 @@
 function wynik_dla_temperatury = uwzglednij_zakres_czestotliwosci(wynik, numer_pomiaru, liczba_pomiarow, min_czestotliwosc, max_czestotliwosc)
 
-liczba_elementow = 0;
-
 for licznik = 1:liczba_pomiarow    
-    
-    if (wynik(numer_pomiaru).impedancja.czestotliwosc(licznik) < min_czestotliwosc) || (wynik(numer_pomiaru).impedancja.czestotliwosc(licznik) > max_czestotliwosc)
+        
+    if wynik(numer_pomiaru).impedancja.czestotliwosc(licznik) < min_czestotliwosc
         
         wynik(numer_pomiaru).impedancja.czestotliwosc(licznik) = NaN;
         wynik(numer_pomiaru).impedancja.Z_exp(licznik) = NaN;
         wynik(numer_pomiaru).impedancja.Z_sym(licznik) = NaN;
+        
+    elseif wynik(numer_pomiaru).impedancja.czestotliwosc(licznik) > max_czestotliwosc
+        
+        wynik(numer_pomiaru).impedancja.czestotliwosc(licznik) = NaN;
+        wynik(numer_pomiaru).impedancja.Z_exp(licznik) = NaN;
+        wynik(numer_pomiaru).impedancja.Z_sym(licznik) = NaN;
+        
     end
-   
+    
 end
+
+wiersze_poza_zakresem = any(isnan(wynik(numer_pomiaru).impedancja.czestotliwosc),2);
+wynik(numer_pomiaru).impedancja.czestotliwosc(wiersze_poza_zakresem,:) = [];
+wynik(numer_pomiaru).impedancja.Z_sym(wiersze_poza_zakresem,:) = [];
+wynik(numer_pomiaru).impedancja.Z_exp(wiersze_poza_zakresem,:) = [];
 
 wynik_dla_temperatury = wynik(numer_pomiaru);
 
